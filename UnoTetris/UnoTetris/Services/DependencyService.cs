f64a4d332a8b84bf8d685b27d056255bc9d9602d
Pedro Jesus <pedrojesus.cefet@gmail.com>
2023-02-23 00:16:23 -0300

    created input service to organize the code

---
+++ UnoTetris/UnoTetris/Services/DependencyService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace UnoTetris.Services;
+public static class DependencyService
+{
+	static readonly ConcurrentDictionary<Type, object> dependencyImplementations = new();
+
+	public static void RegisterSingleton<T, TImpl>(TImpl instance)
+			where T : class
+			where TImpl : class, T
+	{
+		var type = typeof(T);
+
+		if (dependencyImplementations.ContainsKey(type))
+			return;
+
+		dependencyImplementations.TryAdd(type, instance);
+	}
+
+	public static T? Get<T>()
+		where T : class
+	{
+		if (dependencyImplementations.TryGetValue(typeof(T), out var instance))
+		{
+			return (T)instance;
+		}
+
+		return null;
+	}
+
+	class DependencyData
+	{
+		public object GlobalInstance { get; set; }
+
+		public Type ImplementorType { get; set; }
+	}
+
+}
+
---
+++ UnoTetris/UnoTetris/Services/IInputService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using Windows.System;
+
+namespace UnoTetris.Services;
+
+public interface IInputService
+{
+	void ProcessKeyDown(MainPage page, Action<VirtualKey> action);
+}
